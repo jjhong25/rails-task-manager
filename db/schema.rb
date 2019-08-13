02542393e67f7bb038f21d4fe149a991c20a68d4
JJ Hong <jjhong825@gmail.com>
2019-08-13 17:45:12 +0200

    task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_08_13_095854) do
-
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
@@ -19,5 +18,4 @@ ActiveRecord::Schema.define(version: 2019_08_13_095854) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
